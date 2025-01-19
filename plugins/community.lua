b8390f8fc21804abf19a2fe7c0e9755d32b1e045
jashka34 <a_cherniadiev@mail.ru>
2025-01-19 17:16:33 +0300

    Update community.lua for AstroNvim v3

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,15 +1,20 @@
-return {
-  'AstroNvim/astrocommunity',
-  { import = 'astrocommunity.colorscheme.catppuccin' },
-  { import = 'astrocommunity.colorscheme.dracula' },
-  { import = 'astrocommunity.colorscheme.gruvbox' },
-  { import = 'astrocommunity.colorscheme.gruvbox-baby' },
-  { import = 'astrocommunity.colorscheme.monokai-pro' },
-  { import = 'astrocommunity.colorscheme.onigiri' },
-  { import = 'astrocommunity.colorscheme.everforest' },
-  { import = 'astrocommunity.colorscheme.github-nvim-theme' },
-  { import = 'astrocommunity.colorscheme.nord-nvim' },
-  { import = 'astrocommunity.colorscheme.tokyonight' },
-  { import = 'astrocommunity.colorscheme.kanagawa' },
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
 
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.clojure" },
+  --{ "nvim-treesitter-sexp", enabled = false },
+  { "nvim-parinfer", enabled = false },
+  --{ import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.colorscheme.aurora" },
+  { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
 }
